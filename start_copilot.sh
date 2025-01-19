1090d8825cc33178c36ba5bd65269fb2a1f1ceed
s53zo <44831766+s53zo@users.noreply.github.com>
2025-01-19 22:34:09 +0100

    Update start_copilot.sh

--- start_copilot.sh
+++ start_copilot.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-python3 /opt/hamradio-copilot/connect.py -c mqtt -r 499,497,503,504,206,239 & python3 /opt/hamradio-copilot/analyzeSNR.py -o /opt/hamradio-copilot/html/
+python3 /opt/hamradio-copilot/webSNR.py -o /opt/hamradio-copilot/html/
+python3 /opt/hamradio-copilot/webSNR_S53M.py -o /opt/hamradio-copilot/html/
+
+# python3 /opt/hamradio-copilot/connect.py -c mqtt -r 499,497,503,504,206,239 & python3 /opt/hamradio-copilot/analyzeSNR.py -o /opt/hamradio-copilot/html/
